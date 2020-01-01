f4e0c5c085313ee689a1e8101c502e4c5eade971
HADAPC <bishalhada@gmail.com>
2020-01-01 20:50:52 +0545

    routine of multiple days commit

--- app/src/main/java/com/example/finalassignment/fragment/FridayFragment.java
+++ app/src/main/java/com/example/finalassignment/fragment/FridayFragment.java
@@ -4,12 +4,24 @@ package com.example.finalassignment.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.finalassignment.R;
+import com.example.finalassignment.adapter.DayAdapter;
+import com.example.finalassignment.api_classes.BaseAPI;
+import com.example.finalassignment.api_classes.Interface;
+import com.example.finalassignment.modal.RoutineModal;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,16 +29,45 @@ import com.example.finalassignment.R;
 public class FridayFragment extends Fragment {
 
 
+    private static String day = "Monday";
+
     public FridayFragment() {
         // Required empty public constructor
     }
+
+    RecyclerView recyclerView_monday;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friday, container, false);
+        View view = inflater.inflate(R.layout.fragment_monday, container, false);
+        recyclerView_monday = view.findViewById(R.id.recyclerView_Monday);
+        routine_friday();
+        return view;
+    }
+
+    public void routine_friday(){
+        Interface retrofitInterface = BaseAPI.getRetrofit().create(Interface.class);
+        Call<List<RoutineModal>> productModalCall = retrofitInterface.parseRoutine();
+        productModalCall.enqueue(new Callback<List<RoutineModal>>() {
+            @Override
+            public void onResponse(Call<List<RoutineModal>> call, Response<List<RoutineModal>> response) {
+                DayAdapter DayAdapter = new DayAdapter(getActivity(),response.body());
+                LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(getActivity(),
+                        LinearLayoutManager.HORIZONTAL, false);
+                recyclerView_monday.setLayoutManager(horizontalLayoutManager);
+                recyclerView_monday.setHasFixedSize(true);
+                recyclerView_monday.setAdapter(DayAdapter);
+                DayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<RoutineModal>> call, Throwable t) {
+
+            }
+        });
     }
 
 }
