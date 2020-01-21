a5b4a5fb5f9c3a92af45acfc4888c46562c961b8
HADAPC <bishalhada@gmail.com>
2020-01-21 18:29:22 +0545

    UserProfileUI

---
+++ app/src/main/java/com/example/finalassignment/activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.finalassignment.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.finalassignment.R;
+
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
+
