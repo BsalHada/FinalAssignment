5fbb5abc13d7e2f0d6c15ead748ee8dacd17ca44
HADAPC <bishalhada@gmail.com>
2020-02-01 14:15:46 +0545

    binding in cardview to view profile

--- app/src/main/java/com/example/finalassignment/activity/DashboardActivity.java
+++ app/src/main/java/com/example/finalassignment/activity/DashboardActivity.java
@@ -1,6 +1,7 @@
 package com.example.finalassignment.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import com.example.finalassignment.R;
 public class DashboardActivity extends AppCompatActivity {
 ImageView img_Routine;
 Button btn_login, btn_signup;
+CardView cardView_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +22,20 @@ Button btn_login, btn_signup;
         setContentView(R.layout.activity_dashboard);
 
         img_Routine=findViewById(R.id.img_Routine);
+        cardView_profile = findViewById(R.id.profile);
 
         img_Routine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent= new Intent(getApplicationContext(), RoutineActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardView_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
                 startActivity(intent);
             }
         });
